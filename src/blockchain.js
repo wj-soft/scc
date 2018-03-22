6aa196544f99d97f8c2b20893e58e859135d11c0
wj-soft <wooju_lee@daum.net>
2018-03-22 12:04:55 +0900

    add newBlock API & blockchain.js error issue

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,7 +1,7 @@
 const CryptoJS = require("crypto-js");
 
 class Block {
-  constructor(index, hash, previousHash, timestamp, data){
+  constructor(index, hash, previousHash, timestamp, data) {
     this.index = index;
     this.hash = hash;
     this.previousHash = previousHash;
@@ -9,116 +9,121 @@ class Block {
     this.data = data;
   }
 }
-//hash (http://passwordsgenerator.net/sha256-hash-generator/)
+
 const genesisBlock = new Block(
   0,
-  "B71893FBD0048A2C8B27B9E7108ED788410F8FC4DF8A159BAA6061B867D2EC8A",
+  "2C4CEB90344F20CC4C77D626247AED3ED530C1AEE3E6E85AD494498B17414CAC",
   null,
-  1520419204194,
-  "This is the genesis"
+  1520312194926,
+  "This is the genesis!!"
 );
-
-//블록체인 : 블록의 배열
 
 let blockchain = [genesisBlock];
 
-const getLastBlock = () => {
-  return blockchain[blockchain.length-1];
-};
+const getLastBlock = () => blockchain[blockchain.length - 1];
 
-const getTimeStamp = () => {
-  return new Date().getTime() / 1000;
-};
+const getTimestamp = () => new Date().getTime() / 1000;
 
-const getBlockchain = () => {
-  return blockchain;
-}
+const getBlockchain = () => blockchain;
 
-const createHash = (index, previousHash, timestamp, data) => {
-  return CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data).toString());
-};
+const createHash = (index, previousHash, timestamp, data) =>
+  CryptoJS.SHA256(
+    index + previousHash + timestamp + JSON.stringify(data)
+  ).toString();
 
-const createNewBlock = (data) =>{
+const createNewBlock = data => {
   const previousBlock = getLastBlock();
-  const newBlockIndex = previousBlock.index +1;
-  const newTimestame = getTimeStamp();
-  const newHash = createHash(newBlockIndex, previousBlock.hash, newTimestame, data);
+  const newBlockIndex = previousBlock.index + 1;
+  const newTimestamp = getTimestamp();
+  const newHash = createHash(
+    newBlockIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
   const newBlock = new Block(
     newBlockIndex,
     newHash,
     previousBlock.hash,
-    newTimestame,
+    newTimestamp,
     data
   );
   addBlockToChain(newBlock);
   return newBlock;
 };
 
-const getBlockHash = (block) => {
-  return createHash(block.index, block.previousHash, block.timestamp, block.date);
-}
+const getBlocksHash = block =>
+  createHash(block.index, block.previousHash, block.timestamp, block.data);
 
-const isNewBlockValid = (candidateBlock, latestBlock) =>{
-  if(!isNewStructureValid(candidateBlock)){
-    console.log("The candidate block structur is not valid")
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+  if (!isNewStructureValid(candidateBlock)) {
+    console.log("The candidate block structure is not valid");
     return false;
-  } else if(latestBlock.index +1 !== candidateBlock.index){
-    console.log("The candidate block doesnt have a valid index")
+  } else if (latestBlock.index + 1 !== candidateBlock.index) {
+    console.log("The candidate block doesnt have a valid index");
     return false;
-  } else if(latestBlock.hash !== candidateBlock.previousHash){
-    console.log("The previousHash of the candidate block is not the hash of the latest block");
+  } else if (latestBlock.hash !== candidateBlock.previousHash) {
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
     return false;
-  } else if(getBlockHash() !== candidateBlock.hash){
+  } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
     console.log("The hash of this block is invalid");
     return false;
   }
   return true;
-}
+};
 
-const isNewStructureValid = (block) => {
+const isNewStructureValid = block => {
   return (
-    typeof block.index === 'number' && 
-    typeof block.hash === "string" && 
-    typeof block.timestamp == "number" && 
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timestamp === "number" &&
     typeof block.data === "string"
   );
 };
 
-const isChainValid = (candidateChain) =>{
-  const isGenesisValid = (block)=> {
+const isChainValid = candidateChain => {
+  const isGenesisValid = block => {
     return JSON.stringify(block) === JSON.stringify(genesisBlock);
   };
-  if(!isGenesisValid(candidateChain[0])){
-    console.log("The candidateChains`s genesisBlock is not the same as our genesisBlock");
-    return false
+  if (!isGenesisValid(candidateChain[0])) {
+    console.log(
+      "The candidateChains's genesisBlock is not the same as our genesisBlock"
+    );
+    return false;
   }
-  for(let i = 1; i < candidateChain.length; i++){
-    if(!isNewBlockValid(candidateChain[i], candidateChain[i-1])){
+  for (let i = 1; i < candidateChain.length; i++) {
+    if (!isNewBlockValid(candidateChain[i], candidateChain[i - 1])) {
       return false;
     }
   }
   return true;
 };
 
-const replaceChain = (candidateChain) => {
-  if(isChainValid(newChain) && newChain.length > getBlockchain().length){
-    blockchain = newChain;
+const replaceChain = candidateChain => {
+  if (
+    isChainValid(candidateChain) &&
+    candidateChain.length > getBlockchain().length
+  ) {
+    blockchain = candidateChain;
     return true;
   } else {
     return false;
   }
 };
 
-const addBlockToChain = (candidateBlock) => {
-  if(isNewBlockValid(newBlock, getLastBlock())){
-    getBlockchain().push(candidateBlock);
+const addBlockToChain = candidateBlock => {
+  if (isNewBlockValid(candidateBlock, getLastBlock())) {
+    blockchain.push(candidateBlock);
     return true;
-  } else{
+  } else {
     return false;
   }
-}
+};
 
 module.exports = {
   getBlockchain,
   createNewBlock
-}
+};
--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const morgan = require("morgan")
-const Blockchain = require("./blockchain")
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  Blockchain = require("./blockchain");
 
 const { getBlockchain, createNewBlock } = Blockchain;
 
 const PORT = 3000;
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(morgan("combined"));
+
+app.get("/blocks", (req, res) => {
+  res.send(getBlockchain());
+});
+
+app.post("/blocks", (req, res) => {
+  const { body: { data } } = req;
+  const newBlock = createNewBlock(data);
+  res.send(newBlock);
+});
+
 app.listen(PORT, ()=> console.log("서버 실행 중", PORT));
